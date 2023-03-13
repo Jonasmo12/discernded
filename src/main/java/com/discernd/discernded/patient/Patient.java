5fc609c0bda41ea5d5e6d409f8710af7ffce54c5
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-13 19:35:59 +0200

    create methods for patient class

--- src/main/java/com/discernd/discernded/patient/Patient.java
+++ src/main/java/com/discernd/discernded/patient/Patient.java
@@ -7,5 +7,82 @@ public class Patient {
     private Long identityNumber;
     private String email;
 
+    public Patient(Long id) {
+        this.id = id;
+    }
 
+    public Patient(String firstName,
+                   String lastName,
+                   Long identityNumber,
+                   String email
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identityNumber = identityNumber;
+        this.email = email;
+    }
+
+    public Patient(Long id,
+                   String firstName,
+                   String lastName,
+                   Long identityNumber,
+                   String email
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identityNumber = identityNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", identityNumber=" + identityNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(Long identityNumber) {
+        this.identityNumber = identityNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
