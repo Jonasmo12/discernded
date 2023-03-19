f9ff1d459d558b6988c031f4aff6094662dfa4cb
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-19 22:56:45 +0200

    patient controller init and configuration

---
+++ src/main/java/com/discernd/discernded/patient/PatientController.java
@@ -0,0 +1,27 @@
+package com.discernd.discernded.patient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PatientController {
+    private final PatientRepository patientRepository;
+
+
+    public PatientController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @RequestMapping
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/patients", method = RequestMethod.GET)
+    public String patients(Model model) {
+        model.addAttribute("patients", patientRepository.findAll());
+        return "patients";
+    }
+}
