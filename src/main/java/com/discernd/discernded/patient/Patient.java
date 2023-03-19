913be5bff01e1c0dd8d7e066e8c5d96bc4b8dd66
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-19 22:27:16 +0200

    map patient class to database

--- src/main/java/com/discernd/discernded/patient/Patient.java
+++ src/main/java/com/discernd/discernded/patient/Patient.java
@@ -1,10 +1,53 @@
 package com.discernd.discernded.patient;
 
+import javax.persistence.*;
+
+@Entity(name = "Patient")
+@Table(
+        name = "patient",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "patient_email_unique", columnNames = "email"),
+                @UniqueConstraint(name = "patient_identity_number_unique", columnNames = "identity_number")
+        }
+)
 public class Patient {
+    @Id
+    @SequenceGenerator(
+            name = "patient_sequence",
+            sequenceName = "patient_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "patient_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
+    @Column(
+            name = "identity_number",
+            nullable = false
+    )
     private Long identityNumber;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String email;
 
     public Patient(Long id) {
