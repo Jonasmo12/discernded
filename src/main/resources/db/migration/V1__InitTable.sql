df0973919955e6a92caf56eead0bd0f7dca7c2f5
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-19 21:41:28 +0200

    delete flyway db migration file for versioning

--- src/main/resources/db/migration/V1__InitTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE patient (
-    id BIGSERIAL PRIMARY KEY,
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
-    email TEXT NOT NULL,
-    identityNumber TEXT NOT NULL,
-    unique (identityNumber)
-);
