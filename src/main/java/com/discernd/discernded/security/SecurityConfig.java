36cf04f1bdfb36466820a3357b438451a9d36059
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 13:12:50 +0200

    configure authentication provider

--- src/main/java/com/discernd/discernded/security/SecurityConfig.java
+++ src/main/java/com/discernd/discernded/security/SecurityConfig.java
@@ -1,49 +1,57 @@
 package com.discernd.discernded.security;
 
 
+import com.discernd.discernded.patient.PatientService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PatientService patientService;
 
+    public SecurityConfig(PatientService patientService) {
+        this.patientService = patientService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
-                        .regexMatchers("/").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
+                        .regexMatchers("/")
                         .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
-                .logout((logout) -> logout
+                .csrf().disable()
+                .authorizeRequests()
+                        .regexMatchers("/register")
                         .permitAll()
-                );
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .formLogin();
         return httpSecurity.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider();
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder();
+        provider.setUserDetailsService(patientService);
+        return provider;
     }
 }
