48d316ffc2ad57ae82ca44ba649a76ea2e284514
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 12:34:27 +0200

    configure error message for when user is not found

--- src/main/java/com/discernd/discernded/patient/PatientRepository.java
+++ src/main/java/com/discernd/discernded/patient/PatientRepository.java
@@ -1,6 +1,21 @@
 package com.discernd.discernded.patient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+
+//    @Transactional
+//    @Modifying
+//    @Query("UPDATE Patient a" +
+//    "SET a.enabled = TRUE WHERE a.email = ?1")
+//    int enablePatient(String email);
 }
