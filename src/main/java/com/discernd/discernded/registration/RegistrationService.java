7dfed7a25b1d401d40bf1da38134a85576e37175
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 13:20:53 +0200

    annotate with at service

--- src/main/java/com/discernd/discernded/registration/RegistrationService.java
+++ src/main/java/com/discernd/discernded/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.discernd.discernded.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest registrationRequest) {
         return null;
