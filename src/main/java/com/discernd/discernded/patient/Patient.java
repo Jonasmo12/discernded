2f3fc3b81aa6e02b67f7e9a1497656513946bba1
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 22:47:13 +0200

    set default fields for locked and enabled

--- src/main/java/com/discernd/discernded/patient/Patient.java
+++ src/main/java/com/discernd/discernded/patient/Patient.java
@@ -58,8 +58,8 @@ public class Patient implements UserDetails {
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
     public Patient() {
     }
