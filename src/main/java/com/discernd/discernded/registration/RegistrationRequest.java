96de2ff0b8ffd96bb353a7ca1f4a28eeb4688d6c
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 12:49:56 +0200

    configure registration request class methods and constructor

--- src/main/java/com/discernd/discernded/registration/RegistrationRequest.java
+++ src/main/java/com/discernd/discernded/registration/RegistrationRequest.java
@@ -1,4 +1,63 @@
 package com.discernd.discernded.registration;
 
+import java.util.Objects;
+
 public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String identityNumber;
+    private final String email;
+    private final String password;
+
+    public RegistrationRequest(String firstName, String lastName, String identityNumber, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identityNumber = identityNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrationRequest{" +
+               "firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", identityNumber='" + identityNumber + '\'' +
+               ", email='" + email + '\'' +
+               ", password='" + password + '\'' +
+               '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegistrationRequest that = (RegistrationRequest) o;
+        return Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(identityNumber, that.identityNumber) && Objects.equals(email, that.email) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, identityNumber, email, password);
+    }
 }
