69a2822dd1f8cb1fdc8e2fea21b4b46c93b492d9
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 12:53:49 +0200

    return null within the register method

--- src/main/java/com/discernd/discernded/registration/RegistrationService.java
+++ src/main/java/com/discernd/discernded/registration/RegistrationService.java
@@ -2,5 +2,6 @@ package com.discernd.discernded.registration;
 
 public class RegistrationService {
     public String register(RegistrationRequest registrationRequest) {
+        return null;
     }
 }
