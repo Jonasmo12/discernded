dc36a5d43194733f6ab1c2116859b11249820b0b
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-20 12:35:21 +0200

    find user by email

--- src/main/java/com/discernd/discernded/patient/PatientService.java
+++ src/main/java/com/discernd/discernded/patient/PatientService.java
@@ -3,10 +3,21 @@ package com.discernd.discernded.patient;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PatientService implements UserDetailsService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return patientRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format("email %s not found", email)));
     }
 }
