935279896e1437e4032aa8b478f25035f7ffa92a
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-21 16:15:11 +0200

    configure allergy service

--- src/main/java/com/discernd/discernded/allergy/AllergyService.java
+++ src/main/java/com/discernd/discernded/allergy/AllergyService.java
@@ -1,4 +1,32 @@
 package com.discernd.discernded.allergy;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AllergyService {
+    private final AllergyRepository allergyRepository;
+
+    public AllergyService(AllergyRepository allergyRepository) {
+        this.allergyRepository = allergyRepository;
+    }
+
+    public String addAllergy(Allergy allergy) {
+        boolean allergyExists = allergyRepository.findByName(allergy.getName()).isPresent();
+        if (allergyExists) {
+            throw new IllegalStateException(String.format("%s already exists", allergy.getName()));
+        }
+
+        allergyRepository.save(allergy);
+
+        return "allergy saved";
+    }
+
+    public String createAllergy(Allergy allergy) {
+        return addAllergy(
+                new Allergy(
+                        allergy.getName(),
+                        allergy.getMedicalHistory()
+                )
+        );
+    }
 }
