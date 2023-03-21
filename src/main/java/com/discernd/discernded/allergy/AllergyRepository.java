72dab28cc663ecad56851c2d1f3150ae897c4278
Jonas Moloto <jonasmoloto@Jonass-MacBook-Air.local>
2023-03-21 15:43:09 +0200

    annotate allergy repo with repository

--- src/main/java/com/discernd/discernded/allergy/AllergyRepository.java
+++ src/main/java/com/discernd/discernded/allergy/AllergyRepository.java
@@ -1,9 +1,11 @@
 package com.discernd.discernded.allergy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AllergyRepository extends JpaRepository<Allergy, Long> {
     Optional<Allergy> findByName(String name);
 }
